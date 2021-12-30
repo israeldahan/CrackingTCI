bcda8c6e6313460aff0aec310d682cf73e4492ff
Israel Dahan <id@studiodvd.co.il>
2021-12-30 14:24:06 +0200

    add urlify

--- chapterOne/src/main/java/URLify.java
+++ chapterOne/src/main/java/URLify.java
@@ -1,15 +1,23 @@
-
+import org.jetbrains.annotations.NotNull;
 
 public class URLify {
 
-    public static boolean checkPermutation(String strFirst, String strSec){
-
-        return true;
+    public static @NotNull String URLify(@NotNull String str){
+        StringBuilder sb=new StringBuilder("");
+        for (int index = 0; index < str.length() ; index++) {
+            if (String.valueOf(str.charAt(index)).equals(" ")){
+                sb.append(String.valueOf("%20"));
+            } else {
+                sb.append(String.valueOf(str.charAt(index)));
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        String strOne = "str ";
-        String strTwo = "str";
+        String str = "israel david dahan";
+        String urlify = URLify(str);
+        System.out.println(urlify);
 //        CheckPermutation.checkPermutation(strOne, strTwo)
     }
 
